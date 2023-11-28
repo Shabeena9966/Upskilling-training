00fd7aa1942b163f216dbf611fa2ccbb48098bba
snss <111757675+Shabeena9966@users.noreply.github.com>
2023-11-28 09:22:48 +0530

    Add files via upload

---
+++ DAY7/springboot-kafka-microservices/email-service/src/test/java/com/capg/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
